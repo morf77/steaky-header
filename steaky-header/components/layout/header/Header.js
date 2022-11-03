2d4991556878ea0b2b2ff5eebbddfa3b8a2677b8
morf77 <mhamadreza77@gmail.com>
2022-11-03 20:40:23 +0330

    setting up project

---
+++ steaky-header/components/layout/header/Header.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import Title from '../../UI/Title'
+import classes from './Header.module.css'
+
+function Header (props) {
+    const [ sizeClass , sizeSetter] = useState(false);
+    const scrollHandler = event => {
+        if(event.currentTarget.scrollY>0){
+            sizeSetter(true)
+        }else{
+            sizeSetter(false)
+        }
+        console.log(sizeClass)
+
+    }
+    // if (typeof window !== "undefined"){
+    //     window.addEventListener('scroll',scrollHandler)
+    // }
+    // Window.addEventListener('scroll',scrollHandler)
+    // componentWillMount () {
+    //     window.addEventListener('scroll',scrollHandler)
+    // }
+    useEffect(()=>{
+        window.addEventListener('scroll',scrollHandler)
+    },[])
+    return (
+        <header className={`${classes.header} ${sizeClass && classes.active}`}>
+            <Title>
+                A List Of Books
+            </Title>
+        </header>
+    )
+}
+
+export default Header ;
